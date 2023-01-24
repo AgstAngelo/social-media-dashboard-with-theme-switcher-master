c42446cf7177c12f19628ab2acd00641ab73bee4
AgstAngelo <agstangelo@gmail.com>
2023-01-24 13:11:04 -0300

    feature: mobile first - card style developing.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       colors: {
         //light theme - BG is pure white
         "light-theme": "hsl(230, 22%, 74%)",
-        "Very Pale Blue": "hsl(225, 100%, 98%)",
+        "Very-Pale-Blue": "hsl(225, 100%, 98%)",
         "light-card": "hsl(227, 47%, 96%)",
         "light-text-1": "hsl(228, 12%, 44%)",
         "light-text-2": "hsl(230, 17%, 14%)",
@@ -16,7 +16,7 @@ module.exports = {
         "dark-card": "hsl(228, 28%, 20%)",
         "dark-text": "hsl(228, 34%, 66%)",
         //common
-        "lime-green": "color: hsl(163, 72%, 41%)",
+        "lime-green": "hsl(163, 72%, 41%)",
         "bright-red": "hsl(356, 69%, 56%)",
         "theme-btn-start": "hsl(210, 78%, 56%)",
         "theme-btn-end": "hsl(146, 68%, 55%)",
@@ -27,6 +27,12 @@ module.exports = {
         youTube: "hsl(348, 97%, 39%)",
         attribution: "hsl(228, 45%, 44%)",
       },
+      fontFamily: {
+        "inter": "inter"
+      },
+      letterSpacing: {
+        "custom": ".3rem"
+      }
     }
   },
   plugins: [],
